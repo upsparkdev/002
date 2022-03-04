2a554e7a3516613760e6561e838d18f0e88087eb
Matt Wilkins <matt@wilkinsdesign.co.uk>
2022-03-04 12:20:05 +0000

    About and Contact page views added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang=""en">
+<head>
+    <title>About us</title>
+</head>
+<body>
+<h1>About us</h1>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang=""en">
+<head>
+    <title>Contact us</title>
+</head>
+<body>
+<h1>Contact us</h1>
+</body>
+</html>
